4adeffcde75341d43ac7d61b88c2571f8fe7a571
resteve <resteve@zikzakmedia.com>
2015-12-01 11:19:39 +0100

    Tests

--- tests/test_sale_margin.py
+++ tests/test_sale_margin.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python
-# This file is part sale_margin module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_margin module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleMarginTestCase(unittest.TestCase):
+class SaleMarginTestCase(ModuleTestCase):
     'Test Sale Margin module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_margin')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_margin')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_margin'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleMarginTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_sale_margin.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
